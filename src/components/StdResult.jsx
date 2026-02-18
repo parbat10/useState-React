00c3413ae2f7051d619a4b0fb677ffb0619d3be9
parbat10 <parbatlimbu139@gmail.com>
2026-02-18 15:11:24 +0545

    second commit

--- src/components/StdResult.jsx
+++ src/components/StdResult.jsx
@@ -1,20 +1,29 @@
 import React from 'react'
 
-const StdResult = ({stdData}) => {
-  const name=(stdData.name)
-  const mathMarks=(stdData.mathMarks)
-  const scienceMarks=(stdData.scienceMarks)
-  const physicsMarks=(stdData.physicsMarks)
+const StdResult = ({ stdData }) => {
+  const name = (stdData.name)
+  const mathMarks = (stdData.mathMarks)
+  const scienceMarks = (stdData.scienceMarks)
+  const physicsMarks = (stdData.physicsMarks)
 
-  const mathPercent=(stdData.mathMarks/100)*100;
-  const sciencePercent=(stdData.scienceMarks/75)*100;
-  const physicsPercent=(stdData.physicsMarks/75)*100;
+  const mathStatus = mathMarks >= 30 ? "pass" : "fail";
+  const scienceStatus = scienceMarks >= 28 ? "pass" : "fail";
+  const physicsStatus = physicsMarks >= 28 ? "pass" : "fail";
+
+  const overAllStatus = (mathStatus && scienceStatus && physicsStatus) == "pass" ? "pass" : "fail";
+
+  const mathPercent = (stdData.mathMarks / 100) * 100;
+  const sciencePercent = (stdData.scienceMarks / 75) * 100;
+  const physicsPercent = (stdData.physicsMarks / 75) * 100;
   return (
     <>
-    <h3>Name={name}</h3>
-    <p>Math={mathMarks} in percentage={mathPercent}%</p>
-    <p>Science={scienceMarks} in percentage={sciencePercent}%</p>
-    <p>Physics={physicsMarks} in percentage={physicsPercent}%</p>
+      <div className='std-result'>
+        <h3>Name={name}</h3>
+        <p>Math={mathMarks} in percentage={mathPercent}% status={mathStatus}</p>
+        <p>Science={scienceMarks} in percentage={sciencePercent}% status={scienceStatus}</p>
+        <p>Physics={physicsMarks} in percentage={physicsPercent}% status={physicsStatus}</p>
+        <p>OverAllStatus={overAllStatus}</p>
+      </div>
     </>
   )
 }
